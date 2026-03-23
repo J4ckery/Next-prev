38644b46e01a8657c8710cf44d86609772d1eb1c
J4ckery <112798061+J4ckery@users.noreply.github.com>
2026-03-23 13:50:34 +1300

    Change querySelector to querySelectorAll for wrappers

--- nextprev.js
+++ nextprev.js
@@ -104,8 +104,8 @@
     });
 
     // 👇 HIDE WRAPPERS
-    const prevWrapper = document.querySelector("Previous-link-block");
-    const nextWrapper = document.querySelector("Next-link-block");
+    const prevWrapper = document.querySelectorAll(".previous-link-block");
+    const nextWrapper = document.querySelectorAll(".next-link-block");
 
     if (!prevExists && prevWrapper) {
       prevWrapper.style.display = "none";
